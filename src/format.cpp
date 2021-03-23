b4bdbf18bab7304621c3cd43dee3bcb4bcd2b68c
drachins <djfrachinsky@gmail.com>
2021-03-22 22:50:16 -0600

    defined fortmat function

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,31 @@
 #include <string>
+#include <cmath>
 
 #include "format.h"
 
 using std::string;
+using std::to_string;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    
+    double hours;
+    double minutes;
+
+    string hours_str;
+    string minutes_str;
+    string seconds_str;
+
+    hours = floor(seconds/3600);
+    seconds -= hours*3600;
+
+    minutes = floor(seconds/60);
+    seconds -= minutes*60;
+
+
+    return to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+    
+    }
