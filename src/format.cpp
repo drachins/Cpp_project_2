47e0cd31f4b9a63c0849856e8bb854a15ecaa1b9
drachins <djfrachinsky@gmail.com>
2021-03-30 20:12:30 -0600

    changed declaration format

--- src/format.cpp
+++ src/format.cpp
@@ -12,19 +12,14 @@ using std::to_string;
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
     
-    double hours;
-    double minutes;
-
-    string hours_str;
-    string minutes_str;
-    string seconds_str;
+    double hours, minutes;
+    string hours_str, minutes_str, seconds_str;
 
     hours = floor(seconds/3600);
     seconds -= hours*3600;
 
     minutes = floor(seconds/60);
     seconds -= minutes*60;
-
 
     return to_string(long(hours)) + ":" + to_string(int(minutes)) + ":" + to_string(int(seconds));
     
