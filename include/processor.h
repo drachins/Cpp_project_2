a960f7c5905231fc4ef19d4b4df9b2dc039e29d5
drachins <djfrachinsky@gmail.com>
2021-04-08 16:14:27 -0600

    Added private members and CpuUtilParser() method.

--- include/processor.h
+++ include/processor.h
@@ -1,12 +1,16 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
+#include <vector>
+
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
-
+  std::vector<float> CpuUtilParser();
   // TODO: Declare any necessary private members
  private:
+    float oldcpu_active;
+    float oldcpu_idle;
 };
 
 #endif
