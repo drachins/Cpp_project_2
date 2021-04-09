a20c70d59db33b927a414cd5fcefc0e7d62c9efa
drachins <djfrachinsky@gmail.com>
2021-04-09 16:44:48 -0600

    Removed CpuUtilParser() definition.

--- src/processor.cpp
+++ src/processor.cpp
@@ -13,22 +13,6 @@ using std::stof;
 
 
 // TODO: Return the aggregate CPU utilization
-vector<float> Processor::CpuUtilParser(){
-    float active_jiffs;
-    float idle_jiffs;
-    std::vector<string> jiffs = CpuUtilization();
-
-    for(int i = 0; i < CPUStates::kSteal_; i++){
-        if(i == CPUStates::kIdle_ || i == CPUStates::kIOwait_){
-            idle_jiffs+= stof(jiffs[i]);
-        }
-        else{
-            active_jiffs += stof(jiffs[i]);
-        }
-    }
-
-    return vector<float> {active_jiffs, idle_jiffs};
-}
 
 float Processor::Utilization() { 
 
